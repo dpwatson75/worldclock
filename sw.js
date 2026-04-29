04d342c5c9974ab1b4e74652486c4883ae90255e
dpwatson75 <daniel@dpwatson.com>
2026-04-29 16:20:19 -0400

    Add network-first service worker

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'worldclock-v1';
+const ASSETS = ['./index.html', './icon.png'];
+
+// Pre-cache assets on install, activate immediately
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+// Clean up old caches, take control of all clients immediately
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network first: always try to fetch fresh, update cache, fall back to cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(c => c.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
